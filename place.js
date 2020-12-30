01ce6c80db30a1ea3e508ed4051525e503b3dcb4
rishabh-N <74763186+rishabh-N@users.noreply.github.com>
2020-12-30 22:12:34 +0530

    Add files via upload

---
+++ place.js
@@ -0,0 +1,62 @@
+class Place{
+
+  constructor(x,y,width,height){
+
+     var a ={
+        isStatic:true
+     }
+     
+     this.body = Bodies.rectangle(x,y,width,height,a)
+     this.width = width;
+     this.height = height;
+     World.add(world,this.body)
+         
+        
+    
+  }
+
+   display(){
+
+      rectMode(CENTER)
+      fill("red")
+      rect(this.body.position.x,this.body.position.y,this.width,this.height)
+
+
+
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sketch.js
@@ -0,0 +1,78 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+var rect1,rect2,rect3
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+    
+
+	engine = Engine.create();
+	world = engine.world;
+
+	rect1 = new Place (520,680,20,160);	
+	rect2 = new Place(320,650,20,100);
+    rect3 = new Place(420,650,200,20);
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+   background(0);
+   rectMode(CENTER);
+   Engine.update(engine)
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  rect1.display();
+  rect2.display();
+  rect3.display();
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+
+	Matter.Body.setStatic(packageBody,false);
+    
+  }
+}
+
+
+
